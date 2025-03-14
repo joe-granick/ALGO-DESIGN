e6687d0d2ba3230d06bf61117247f29420c0a751
joe-granick <jmgranick@gmail.com>
2025-03-14 17:18:47 -0400

    faster insertion sort with adaptive search and sentinel key implemented

--- elementary-sort/elementary-sort-driver.c
+++ elementary-sort/elementary-sort-driver.c
@@ -8,13 +8,36 @@ typedef int Item; //how does item work here?
 
 // insertion sort implementation
 
-void sort(Item a[], int l, int r)
+void insertion_sort(Item a[], int l, int r)
   {
     int i, j;
     for(i=l+1; i <= r; i++)
       for(j = i; j > l; j--)
         compexch(a[j-1], a[j]);
   }
+
+
+/*
+ * this version of insertion sort improves upon the previous implementation in 3 ways
+* 1. sentinel key is created by sorting the smallest element into the first index 
+* 2. exchange removed from inner loop, just using a single assignment avoidng the overhead ops of compexch
+* 3. converted to an adpative search terminating inner loop when element inserted 
+* */
+void insertion_sentinel(Item a[], int l, int r)
+{
+  int i;
+
+  // find lowest value for sentinel key, and place in first index
+  for(i=r; i>l; i--) compexch(a[i-1],a[i]);
+
+  for(i=l+2; i<=r; i++)
+  {
+    int j=i; Item key = a[j]
+    while(less(key, a[j-1]))
+      {a[j] = a[j-1]; j--;}
+    a[j] = key;
+  }
+}
 
 int main(int argc, char *argv[])
   { int i, N = atoi(argv[1]), sw = atoi(argv[2]);
